e6fc202f3ce4753e315139292d533d476b8bd8e3
irek <irek@comp-admin.SRP>
2016-03-10 13:13:55 +0300

    step mvc

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn3 -c etc/dj.py ask.wsgi:application --chdir /home/box/web/ask/
