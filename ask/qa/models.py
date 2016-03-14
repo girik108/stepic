8dca6ecc5dd1419e9bccee8f60ec30e414365e79
irek <irek@comp-admin.SRP>
2016-03-14 12:26:13 +0300

    mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User# as DjUser
 
 # Create your models here.
+
+#class User(DjUser):
+#    def __init__(self):
+#        super.__init__(self)
+
+
+class Question(models.Model):   #вопрос
+    title = models.CharField(max_length = 50)  #заголовок вопроса
+    text = models.TextField()                   #полный текст вопроса
+    added_ad = models.DateTimeField()           #дата добавления вопроса
+    rating = models.IntegerField(default = 0)              #рейтинг вопроса (число)
+    author = models.ForeignKey(User)            #автор вопроса
+    likes = models.ManyToManyField(User, related_name="likelike")        #список пользователей,
+                                                #поставивших "лайк"
+#    class Meta:
+#        db_table
+
+class Answer(models.Model):  #ответ
+    text = models.TextField()               #текст ответа
+    added_at = models.DateTimeField()       #дата добавления ответа
+    question = models.ForeignKey(Question)  #вопрос, к которому относится ответ 
+    author = models.ForeignKey(User)        #автор ответа
+
--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn3 -c etc/dj.py ask.wsgi:application --chdir /home/box/web/ask/
+
+sudo service mysql start
+mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY '11111111';"
+mysql -uroot -e "CREATE DATABASE 'qa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON 'qa'.* TO 'django'@'localhost';"
