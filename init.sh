8d7851bd21a13624d48c98aa82344c6fda488c14
irek <irek@comp-admin.SRP>
2016-03-14 12:27:49 +0300

    mysql

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sudo /etc/init.d/gunicorn restart
 sudo gunicorn3 -c etc/dj.py ask.wsgi:application --chdir /home/box/web/ask/
 
 sudo service mysql start
-mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY '11111111';"
+mysql -uroot -e "CREATE USER 'django' IDENTIFIED BY '11111111';"
 mysql -uroot -e "CREATE DATABASE 'qa'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON 'qa'.* TO 'django'@'localhost';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON 'qa'.* TO 'django';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
