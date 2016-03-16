4fcedd75828c00c391149b882bc4f80d2ea3c3c1
girik108 <gimatov@list.ru>
2016-03-16 22:34:54 +0300

    cool2

--- init.sh
+++ init.sh
@@ -1,11 +1,20 @@
+sudo apt-get update && sudo apt-get install mc python-mysqldb -y
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn3 -c etc/dj.py ask.wsgi:application --chdir /home/box/web/ask/
+sudo /etc/init.d/gunicorn stop
+
+sudo pip3 install django==1.8
+sudo pip3 install gunicorn
+
+sudo gunicorn -c etc/dj.py ask.wsgi:application --chdir /home/box/web/ask/
 
 sudo service mysql start
-mysql -uroot -e "CREATE USER 'django' IDENTIFIED BY '11111111';"
-mysql -uroot -e "CREATE DATABASE 'qa'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON 'qa'.* TO 'django';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+
+mysql -uroot -e "CREATE USER 'django1' IDENTIFIED BY '11111111';"
+mysqladmin create qabase -uroot 
+mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO django"
+mysqladmin flush-privileges -uroot 
+
+ask/manage.py makemigrations qa
+ask/manage.py migrate
