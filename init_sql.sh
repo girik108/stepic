94b58e75f8e71e190b4bc761715cc2ded6c4de0f
girik108 <gimatov@list.ru>
2016-03-19 22:36:44 +0300

    qa4

---
+++ init_sql.sh
@@ -0,0 +1,1 @@
+mysql -u root -e "INSERT INTO `auth_user` (`id`,`password`,`last_login`,`is_superuser`,`username`,`first_name`,`last_name`,`email`,`is_staff`,`is_active`,`date_joined`) VALUES (1,'123456','0000-00-00 00:00:00.000000',1,'admin','Admin','Admin','',1,1,'0000-00-00 00:00:00.000000');"
